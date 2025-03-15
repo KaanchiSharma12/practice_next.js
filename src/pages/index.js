3f4301c022089ccf2f1efd51d26b72bb6513adfc
KaanchiSharma12 <kukku.kaanchi@gmail.com>
2025-03-15 16:40:38 +0530

    update changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,9 +15,9 @@ export default function Home() {
   };
   return (
     <div>
-      {/* <HeroBanner overlay={overlay} heroBannerData={heroBannerData} />
+      {/* <HeroBanner overlay={overlay} heroBannerData={heroBannerData} /> */}
       <ColThreeCard />
-      <ImageWithContent /> */}
+      {/* <ImageWithContent /> */}
       <IntroBlock />
       {/* <CaseStudies />
       <Cta />
