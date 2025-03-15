7d4b2e2f2a3304b1cd328ac5ecb3f8c2a4aa6a69
KaanchiSharma12 <kukku.kaanchi@gmail.com>
2025-03-15 17:34:49 +0530

    update

--- src/components/IntroBlock.jsx
+++ src/components/IntroBlock.jsx
@@ -3,7 +3,7 @@ export default function IntroBlock() {
     <section className="IntroBlock">
       <div className="container">
         <div className="introWrap text-center">
-          <h3 className="text-black">What is NextJS used for?s</h3>
+          <h3 className="text-black">What is NextJS used for?</h3>
           <p className="pt-[15px] text-black">
             Next. js is a React framework that enables several extra features, including server-side rendering and static rendering. React is a JavaScript library that is traditionally used to build web applications rendered in the client's browser with JavaScript.
           </p>
@@ -13,7 +13,7 @@ export default function IntroBlock() {
               class="pink-btn link-white text-white"
               data-cursor-expand=""
             >
-              View all case studies
+            Learn More
             </a>
           </div>
         </div>
